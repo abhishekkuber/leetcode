e8d8d4495a19aff0548cbf760dc78e5cadac5e8a
Abhishek Kuber <abhi.kuber@gmail.com>
2021-07-29 10:59:35 +0530

    Added more codes

---
+++ leetcode_215.py
@@ -0,0 +1,37 @@
+'''
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+'''
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        '''
+        Alternate approach
+        nums.sort()
+        return nums[-k]
+        '''
+        
+        for i in range(k):
+            for j in range(len(nums)-i-1):
+                if (nums[j] > nums[j+1]):
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+        
+        return nums[-k]
+        
